e8b44ea7e697057b6b6aa3860d158d0915054f24
Mariola Wolan <mariola@gmail.com>
2025-04-01 03:11:40 +0200

    DB

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,28 +1,30 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from django.conf import settings
-from django.conf.urls.static import static
-from django.urls import re_path
-from django.views.static import serve
-import core.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django.setup() # Initialize Django settings FIRST
 
-# Obsługa statycznych plików (np. DRF CSS/JS) – tylko w DEBUG
-static_urlpatterns = []
-if settings.DEBUG:
-    static_urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# Get the application instance *after* setup
+django_asgi_app = get_asgi_application()
 
+# Import Channels items *after* setup
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+# Define a function to get the websocket application
+# This delays the import of core.routing until the function is called
+def get_websocket_app():
+    import core.routing # Import routing here
+    return AuthMiddlewareStack(
+        URLRouter(
+            core.routing.websocket_urlpatterns
+        )
+    )
+
+# Define the application router
 application = ProtocolTypeRouter({
-    "http": URLRouter(
-        static_urlpatterns + [
-            re_path(r"", get_asgi_application()),  # obsługa pozostałych routów Django
-        ]
-    ),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(core.routing.websocket_urlpatterns)
-    ),
+    "http": django_asgi_app,
+    # Call the function to get the websocket app, delaying imports
+    "websocket": get_websocket_app(),
 })
-
